c684f06642970152884cc6419bde9547f20ecc0b
Hvbert1 <henryjiafu@gmail.com>
2023-11-08 18:37:56 +1100

    test then create 'isSunk' function

--- src/factories/createShip.js
+++ src/factories/createShip.js
@@ -2,11 +2,17 @@ const createShip = (length, hitTimes = 0, sunk = false) => {
     const hit = function () {
       this.hitTimes++;
     }
+    const isSunk = function() {
+        if (this.hitTimes === this.length) {
+            this.sunk = true;
+        }
+    }
     return {
       length: length,
       hitTimes: hitTimes,
       sunk: sunk,
       hit: hit,
+      isSunk: isSunk,
     }
   };
   
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -7,3 +7,12 @@ test('hit marker', () => {
 
     expect(carrier.hitTimes).toBe(1);
 });
+
+test('create sunk ship', () => {
+    const carrier = createShip(5);
+    for (let i = 0; i < carrier.length; i++) {
+        carrier.hit();
+    }
+    carrier.isSunk();
+    expect(carrier.sunk).toBeTruthy();
+});
