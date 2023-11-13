53b5df4f9826277ff01428f532a6ad94df95bf1d
Hvbert1 <henryjiafu@gmail.com>
2023-11-13 13:21:13 +1100

    populate + render gameboard

--- src/factories/dom.js
+++ src/factories/dom.js
@@ -1,3 +1,7 @@
+const createBoard = require('../factories/createBoard');
+const mainGameLoop = require('../factories/game');
+
+
 function loadHeader() {
     let headerContainer = document.createElement("div");
     headerContainer.id = "headerContainer";
@@ -11,14 +15,17 @@ function loadHeader() {
     document.getElementById("content").appendChild(headerContainer);
 }
 
+
+const { player, ai } = mainGameLoop();
+
 function loadMain() {
     let mainContainer = document.createElement("div");
     mainContainer.id = "mainContainer";
 
-    let board1 = loadBoard(10);    
+    let board1 = loadBoard(player.board);    
     board1.id = "board1";  
 
-    let board2 = loadBoard(10);
+    let board2 = loadBoard(ai.board);
     board2.id = "board2";  
 
     mainContainer.appendChild(board1);
@@ -27,17 +34,24 @@ function loadMain() {
     document.getElementById("content").appendChild(mainContainer);
 };
 
-function loadBoard(j) {
+function loadBoard(board) {
     let grid = document.createElement("div");
     grid.classList.add("grid");
 
-    for(let i = 0; i < j; i++) {
+    for (let i = 0; i < board.board.length; i++) {
         var row = document.createElement('div');
         row.classList.add('row');
         grid.appendChild(row);
-        for(let k = 0; k < j; k++) {
+        for(let j = 0; j < board.board.length; j++) {
             var cell = document.createElement('div');
+
+            if (board.board[i][j].shipInfo !== null) {
+                cell.id = 'cell-s';
+            } else {
+                cell.id = 'cell-n'
+            }
             cell.classList.add('cell');
+
             cell.innerText = i;
             row.appendChild(cell);
         }
--- src/factories/game.js
+++ src/factories/game.js
@@ -1,9 +1,44 @@
 const { createPlayer, createBot } = require('../factories/createPlayer');
 const createBoard = require('../factories/createBoard');
+const createShip = require('../factories/createShip');
+
 
 const mainGameLoop = () => {
     const playerBoard = createBoard();
     const botBoard = createBoard();
+
+    assignShips(playerBoard);
+    assignShips(botBoard);
+
     const player = createPlayer(playerBoard, 'player');
     const ai = createBot(botBoard, 'ai');
+
+    return { player, ai };
 }
+
+const createShips = () => {
+    const allShips = [];
+
+    const carrier = createShip(5, 0, false);
+    const battleship = createShip(4, 0, false);
+    const destroyer = createShip(3, 0, false);
+    const submarine = createShip(2, 0, false);
+    const patrolBoat = createShip(1, 0, false);
+
+    allShips.push(carrier);
+    allShips.push(battleship);
+    allShips.push(destroyer);
+    allShips.push(submarine);
+    allShips.push(patrolBoat);
+
+    return allShips;
+}
+
+const assignShips = (board) => {
+    for (let i = 0; i < createShips().length; i++) {
+        board.placeShip(createShips()[i], i, 0, 'horizontal')
+    }
+};
+
+module.exports = mainGameLoop;
+
